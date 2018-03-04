ce467680febf3f5c50d3f0c75373b9659a9b4add
J3 <giljr.2009@gmail.com>
2018-03-04 17:30:32 -0400

    Part 7 Unity Game Debut Project - files

---
+++ #7_Part_UnityGameDebut/Crosshair.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //hide the mouse cursor
+        Cursor.visible = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //make the mouse convas follows its position on screen 
+        this.transform.position = Input.mousePosition;		
+	}
+}
