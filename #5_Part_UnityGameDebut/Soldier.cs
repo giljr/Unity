7d0a5a29bbc89cac18d09904e8d7a4090751ea43
J3 <giljr.2009@gmail.com>
2018-03-03 22:03:32 -0400

    5_Part_UnityGameDebut/Soldier.cs
    5_Part_UnityGameDebut/Soldier.cs

---
+++ #5_Part_UnityGameDebut/Soldier.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour {
+    private GameObject mEnemy;
+
+    // Use this for initialization
+    void Start () {
+        //Move to the left
+        mEnemy = transform.GetChild(0).gameObject;
+
+        //Get enemy position and use trig function to move it
+        Vector3 enemyPos = mEnemy.transform.position;
+        enemyPos.z += Mathf.Cos(0.1f);
+
+        iTween.MoveTo(mEnemy, enemyPos, 2.0f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
