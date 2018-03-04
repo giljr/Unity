96cb838fae5153a8f8259df489a23ab86a8579a2
J3 <giljr.2009@gmail.com>
2018-03-04 13:20:35 -0400

    Add files via upload

---
+++ #6_Part_UnityGameDebut/Soldier.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldier : MonoBehaviour {
+    private GameObject mEnemy;
+
+
+    public float RightTime = 1.0f;
+    public float ShootTime = 1.0f;
+    public float LeftTime = 1.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Get the enemy actual gameObject
+        mEnemy = transform.GetChild(0).gameObject;
+        Activate();
+
+       
+    }
+
+    public void Activate()
+    {
+        MoveLeftwards();
+        //return to the right position and stayd there
+        Invoke("MoveRightwards", RightTime);
+
+    }
+
+    private void MoveLeftwards()
+    {
+       
+        //Get enemy position and use trig function to move it
+        Vector3 enemyPos = mEnemy.transform.position;
+        enemyPos.z += Mathf.Cos(0.1f);
+
+        iTween.MoveTo(mEnemy, enemyPos, LeftTime);
+    }
+
+    private void MoveRightwards()
+    {
+        //Move to the left
+        mEnemy = transform.GetChild(0).gameObject;
+
+        //Get enemy position and use trig function to move it
+        Vector3 enemyPos = mEnemy.transform.position;
+        enemyPos.z -= Mathf.Cos(-0.1f);
+
+        iTween.MoveTo(mEnemy, enemyPos, ShootTime);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
